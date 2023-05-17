10de78de1a09c0634425c09eaacce469cf86f8b3
theusindabike <mlopes.matheus@gmail.com>
2023-05-17 17:14:38 -0300

    longestSubstringWithKDistinctChars

--- exercicies.py
+++ exercicies.py
@@ -1,9 +1,10 @@
 import unittest
 
-a = [2, 1, 5, 1, 3, 2]
-b = [2, 3, 4, 1, 5]
 
 class TestingStuff(unittest.TestCase):
+
+    a = [2, 1, 5, 1, 3, 2]
+    b = [2, 3, 4, 1, 5]
 
     def maxSumOfContiguousSubArray(arr: list[int], k: int) -> int:
         maxSum, windowSum, windowStart = 0, 0, 0
@@ -14,7 +15,7 @@ class TestingStuff(unittest.TestCase):
                 windowSum -= arr[windowStart]
                 windowStart += 1
         return maxSum
-
+    # https://dvpr.gitbook.io/coding-interview-patterns/1.-pattern-sliding-window/1.2-smallest-subarray-with-a-given-sum-easy
     def test_smallestSubarrayWithGivenSum(self):
         c = [2, 1, 5, 2, 3, 2]
         d = [2, 1, 5, 2, 8]
@@ -36,26 +37,33 @@ class TestingStuff(unittest.TestCase):
         result = smallestSubarrayWithGivenSum(d, 7)
         self.assertEqual(result, 1)
 
+    # https://dvpr.gitbook.io/coding-interview-patterns/1.-pattern-sliding-window/1.3-longest-substring-with-k-distinct-characters-medium
     def test_longestSubstringWithKDistinctChars(self):
         f = 'araaci'
         g = 'cbbebi'
-
         def longestSubstringWithKDistinctChars(target_text: str, k: int) -> int:
             maxLength, windowStart = 0, 0
-            result = {}
+            d = {}
 
-            for windowEnd, character in enumerate(target_text):
-                result[character] = result[character] + 1 if character in result else 1
-                print(result)
+            for windowEndCharacter, character in enumerate(target_text):
+                d[character] = d[character] + 1 if character in d else 1
+                print('d: ', d)
+                print('character: ', character)
+                print('maxLength: ', maxLength)
 
-                while(len(result) > k):
-                    result[target_text[windowStart]] -= 1
+                while(len(d) > k):
+                    windowStartCharacter = target_text[windowStart]
+                    d[windowStartCharacter] -= 1
 
-                    if(result[character] == 0):
-                        del result[character]
+                    print('[before][while-treatement] d: ', d)
+                    if(d[character] == 0):
+                        del d[character]
                     windowStart += 1
+                    print('[after][while-treatement] d: ', d)
+                    print('windowStart: ', windowStart)
                 
-                maxLength = max(maxLength, windowEnd - windowStart + 1)
+                maxLength = max(maxLength, windowEndCharacter - windowStart + 1)
+                print('--------end_for--------')
 
             return maxLength
 
